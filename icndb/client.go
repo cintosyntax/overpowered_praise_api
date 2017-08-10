8ee7ed9478cd03595e8ff03bee148363cc2a22a2
edwin <edwin@homerlogistics.com>
2017-08-10 00:44:13 -0400

    Add API functionality to this endpoint

---
+++ icndb/client.go
@@ -0,0 +1,48 @@
+package icndb
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type response struct {
+	Type  string        `json:"type,omitempty"`
+	Value responseValue `json:"value,omitempty"`
+}
+
+type responseValue struct {
+	ID         int      `json:"id,omitempty"`
+	Joke       string   `json:"joke,omitempty"`
+	Categories []string `json:"categories,omitempty"`
+}
+
+const (
+	baseURL = "https://api.icndb.com"
+)
+
+// GetRandomJoke fetches a joke from the ICNDB API (http://www.icndb.com/api/) and
+// and returns the resultant string w/o escaped characters.
+func GetRandomJoke(firstName string, lastName string) string {
+	// Defer replacing 'Chuck Norris' to after the call has been made.
+	url := baseURL + "/jokes/random?escape=javascript"
+
+	resp, _ := http.Get(url)
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var r response
+	json.Unmarshal(body, &r)
+
+	joke := r.Value.Joke
+
+	// Clean up the joke by removing Norris or Chuck if a name was given.
+	if firstName != "" || lastName != "" {
+		joke = strings.Replace(joke, "Chuck ", firstName, -1)
+		joke = strings.Replace(joke, "Chuck ", "", -1)
+		joke = strings.Replace(joke, "Norris", lastName, -1)
+		joke = strings.Replace(joke, "Norris", "", -1)
+	}
+
+	return joke
+}
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/cintosyntax/overpowered_praise_api/icndb"
+)
+
+const (
+	defaultPort         = "9000"
+	serviceName         = "Overpowered Praise API"
+	defaultResponseType = "in_channel"
+)
+
+type slackResponseBody struct {
+	ResponseType string `json:"response_type,omitempty"`
+	Text         string `json:"text,omitempty"`
+}
 
 func main() {
-	fmt.Println("Hello")
+	// joke := icndb.GetRandomJoke("Edwin", "Mak")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+
+		praise := icndb.GetRandomJoke(name, "")
+		apiResponse := &slackResponseBody{
+			ResponseType: defaultResponseType,
+			Text:         praise,
+		}
+
+		json.NewEncoder(w).Encode(apiResponse)
+	})
+
+	fmt.Println(serviceName + " serving on port :" + defaultPort)
+	log.Fatal(http.ListenAndServe(":"+defaultPort, nil))
 }
